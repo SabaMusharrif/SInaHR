324862721909c5b3318cd0304e2da6ece5f1f756
SabaMusharrif <sabamusharrif@gmail.com>
2024-05-02 02:15:45 +0500

    HR-Completed
    Completed 2May24

---
+++ story_content/5e777S4t5Cb_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.86.32080.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.004%0D%0ACompensatory%20Leaves,%20click%20to%20learn%20about%20each.%0D%0A%0D%0A"}')
