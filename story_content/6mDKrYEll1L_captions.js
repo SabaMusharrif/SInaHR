3c04651c96c8fb70378fbd84c3d859f4cef3cd0d
SabaMusharrif <sabamusharrif@gmail.com>
2024-05-02 02:32:27 +0500

    completed-fixed
    fixes uploaded

--- story_content/6mDKrYEll1L_captions.js
+++ story_content/6mDKrYEll1L_captions.js
@@ -1,1 +1,1 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.85.31840.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:01.985%0D%0AMake%20sure%20volume%20is%20turned%20up.%0D%0A%0D%0A"}')
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.86.32080.0%0D%0A%0D%0A00:00:00.187%20--%3E%2000:00:02.299%0D%0AEnsure%20that%20the%20volume%20is%20turned%20up.%0D%0A%0D%0A"}')
